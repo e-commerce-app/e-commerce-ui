8c02f3a13de2b4702bbc876e86dd3aabb96101a7
Igor Kulishov <igorkulishov@gmail.com>
2018-12-08 13:31:33 -0500

    story-2: 1) renamed component 2) added hash identifier 3) added FormModule 4) added form and submit

--- e-commerce-project/src/app/app.module.ts
+++ e-commerce-project/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputPageComponent } from './input-page/input-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputPageComponent
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
